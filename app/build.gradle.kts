67a1a7c12a85e4fb280400a424796e9639906a5c
Koang Biel <koangbiel23@gmail.com>
2023-08-24 17:57:55 +0530

    changing the zomato logo scale

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.hbb20:ccp:2.7.3")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 
 }
